1ee156d3c5c3773e1f206615f0dcb1ce5a7b9a56
danna <dannavila.1809@gmail.com>
2023-01-13 16:03:39 -0500

    Add Frontend Mentor starter files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Bill
+
+      Select Tip %
+      5%
+      10%
+      15%
+      25%
+      50%
+      Custom
+
+      Number of People
+
+      Tip Amount
+      / person
+
+      Total
+      / person
+
+      Reset
+
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Your Name Here</a>.
+      </div>
     </div>
   );
 }
